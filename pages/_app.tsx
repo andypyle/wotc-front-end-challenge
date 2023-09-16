882fdaca2d5ae01814da76a2536aa3cd51efb4f1
Andy Pyle <andypyle@Andys-MacBook-Pro-2.local>
2023-09-16 10:18:28 -0700

    Removed some generated stuff. Added emotion. Added global styles.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
+import { globalStyles } from '@/styles/styles'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {globalStyles}
+      <Component {...pageProps} />
+    </>
+  )
 }
